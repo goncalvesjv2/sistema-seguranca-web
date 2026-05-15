8f834e54a84d8f651e4375a273fdd29c4db7bfee
goncalvesjv2 <joaogoncalves3m@gmail.com>
2026-05-15 15:42:49 -0300

    feat: add zod validation

---
+++ backend/src/middlewares/validate.js
@@ -0,0 +1,13 @@
+export const validate = (schema) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) { 
+        return res.status(400).json({ errors: error.issues});
+    }
+
+    req.body = result.data;
+
+    next();
+  }
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { register, login } from '../controllers/authController.js';
+import { registerSchema, loginSchema } from '../validations/authValidation.js';    
+import { validate } from '../middlewares/validate.js';
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validate(registerSchema), register);
+router.post('/login', validate(loginSchema), login);
 
 export default router;
